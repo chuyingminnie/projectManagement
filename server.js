3c1d60522951b9ff443dd049062c1765bc4af6a9
Micky Chan <mickychanhk@outlook.com>
2020-08-29 15:14:36 +0800

    create another structure of express framework

--- server.js
+++ server.js
@@ -1,14 +1,47 @@
-const path = require("path");
-const express = require("express");
-const ejs = require("ejs");
-const bodyParser = require("body-parser");
-const mysqlConnection = require("./connection");
-const indexRoutes = require("./routes/index");
+const mongoose = require('mongoose');
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config({
+    path: './config.env'
+});
 
-var app = express();
-app.use(bodyParser.json());
+process.on('uncaughtException', err => {
+    console.log('UNCAUGHT EXCEPTION!!! shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1);
+});
 
-app.use("/index",indexRoutes);
+const app = require('./app');
 
+const database = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
-app.listen(3000);
+db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
+  })
+// Connect the database
+// mongoose.connect(database, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+// }).then(con => {
+//     console.log('DB connection Successfully!');
+// });
+
+// Start the server
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log(`Application is running on port ${port}`);
+});
+
+process.on('unhandledRejection', err => {
+    console.log('UNHANDLED REJECTION!!!  shutting down ...');
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
+});
+
+module.exports = app;
